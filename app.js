95abccd990bb9dd8087f5c26fdb340c21ca64995
IT-Asraf <itasrafhossain@gmail.com>
2019-12-11 19:30:32 +0600

    Another Module

--- app.js
+++ app.js
@@ -10,7 +10,28 @@ var budgetController = (function(){
     //Public Area
     return{
         publicTest : function(b){
-            console.log(add(b));
+            return add(b);
         }
     }
 })();
+var c = budgetController.publicTest(50);
+console.log(c);
+var UIController = (function(){
+    
+    //Some Code
+
+})();
+
+var controller = (function(budgetCtrl, UICtrl){
+
+    //budgetController.publicTest(10);
+    var z = budgetCtrl.publicTest(10);
+    return{
+        anotherPublic: function(){
+            console.log(z);
+        }
+    }
+})(budgetController, UIController);
+
+
+controller.anotherPublic();
