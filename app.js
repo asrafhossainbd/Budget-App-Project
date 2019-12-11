ee475d99075964213d367ad688caa96f52492e79
IT-Asraf <itasrafhossain@gmail.com>
2019-12-11 19:09:41 +0600

    Module Pattern

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
+// Implementing the Module Pattern
 
+var budgetController = (function(){
+    //Private Area
+    var x = 23;
 
-
-var button = document.querySelector('.add__btn')
-var count=0;
-button.addEventListener('click', function(){
-        console.log(count++ + ' Enter was pressed')
-    })
-
-    
-document.addEventListener('keypress', function(e){
-
-        if(e.keycode === 13 || e.which === 13){
-             console.log( count++ + ' Enter was pressed')
-         }
-})
+    var add = function(a){
+        return x + a;
+    }
+    //Public Area
+    return{
+        publicTest : function(b){
+            console.log(add(b));
+        }
+    }
+})();
