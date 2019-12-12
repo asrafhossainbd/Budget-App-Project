184c0d069133796972852d9c0e821d07faccfa82
IT-Asraf <itasrafhossain@gmail.com>
2019-12-12 15:50:57 +0600

    Creating Income and Expense Function Constructors

--- app.js
+++ app.js
@@ -156,13 +156,115 @@
 // })(budgetController, UIController);
 
 
-//Creating an Initialization Function
-//***********************************/
+// //Creating an Initialization Function
+// //***********************************/
+
+// //Budget Controller
+// var budgetController = (function(){
+    
+//     //Some Code
+
+// })();
+
+
+// //UI Controller
+// var UIController = (function(){
+//     var DOMStrings = {
+//         inputType : '.add__type',
+//         inputDescription : '.add__description',
+//         inputValue : '.add__value',
+//         inputBtn : '.add__btn'
+
+//     }
+
+
+//     return{
+//         getInput: function(){
+//             return{
+//                 type : document.querySelector(DOMStrings.inputType).value, //Will be either inc or exp
+//                 description : document.querySelector(DOMStrings.inputDescription).value,
+//                 value : document.querySelector(DOMStrings.inputValue).value
+//             };
+//         },
+//         getDOMStrings: function(){
+//             return DOMStrings;
+//         }
+//     }
+
+// })();
+
+
+
+// //Global App Controller
+// var controller = (function(budgetCtrl, UICtrl){
+
+//     var setupEventListoners = function(){
+
+//         document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem)
+    
+//         document.addEventListener('keypress', function(event){
+//             if(event.keycode === 13 || event.which === 13){
+//                 ctrlAddItem();
+//             }
+//         });
+//     };
+
+//     var DOM = UICtrl.getDOMStrings();
+//     //For DRY Code
+//     var ctrlAddItem = function(){
+        
+//         //1. Get the field input data
+//         var input = UICtrl.getInput();
+//         //2. Add the item to the budget controller
+
+//         //3. Add the item to the UI
+
+//         //4. Calculate the budget
+
+//         //5. Display the budget on the UI
+
+//     };
+
+//     return{
+//         init: function(){
+//             console.log('Application has started.')
+//             setupEventListoners();
+//         }
+//     }
+
+ 
+// })(budgetController, UIController);
+
+// controller.init();
+
+//Creating Income and Expense Function Constructors
+//**************************************************/
 
 //Budget Controller
 var budgetController = (function(){
     
-    //Some Code
+    var Expense = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
+
+    var Income = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    } 
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    }
 
 })();
 
