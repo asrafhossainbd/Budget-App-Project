86461813db7a6ac63ebce19de7bfb6dbef1d6e75
IT-Asraf <itasrafhossain@gmail.com>
2019-12-12 11:32:49 +0600

    Global App Controller

--- app.js
+++ app.js
@@ -1,37 +1,87 @@
 // Implementing the Module Pattern
 
-var budgetController = (function(){
-    //Private Area
-    var x = 23;
+// var budgetController = (function(){
+//     //Private Area
+//     var x = 23;
 
-    var add = function(a){
-        return x + a;
-    }
-    //Public Area
-    return{
-        publicTest : function(b){
-            return add(b);
-        }
-    }
+//     var add = function(a){
+//         return x + a;
+//     }
+//     //Public Area
+//     return{
+//         publicTest : function(b){
+//             return add(b);
+//         }
+//     }
+// })();
+// var c = budgetController.publicTest(50);
+// console.log(c);
+// var UIController = (function(){
+    
+//     //Some Code
+
+// })();
+
+// var controller = (function(budgetCtrl, UICtrl){
+
+//     //budgetController.publicTest(10);
+//     var z = budgetCtrl.publicTest(10);
+//     return{
+//         anotherPublic: function(){
+//             console.log(z);
+//         }
+//     }
+// })(budgetController, UIController);
+
+
+// controller.anotherPublic();
+
+
+
+//Setting up the First Event Listeners
+//************************************/
+
+//Budget Controller
+var budgetController = (function(){
+    
+    //Some Code
+
 })();
-var c = budgetController.publicTest(50);
-console.log(c);
+
+
+//UI Controller
 var UIController = (function(){
     
     //Some Code
 
 })();
 
+
+
+//Global App Controller
 var controller = (function(budgetCtrl, UICtrl){
 
-    //budgetController.publicTest(10);
-    var z = budgetCtrl.publicTest(10);
-    return{
-        anotherPublic: function(){
-            console.log(z);
+    document.querySelector('.add__btn').addEventListener('click', function(){
+
+        //1. Get the field input data
+
+        //2. Add the item to the budget controller
+
+        //3. Add the item to the UI
+
+        //4. Calculate the budget
+
+        //5. Display the budget on the UI
+
+        console.log('Clicked');
+    })
+    
+    document.querySelector('.add__btn').addEventListener('keypress', function(event){
+        if(event.keycode === 13 || event.which === 13){
+            console.log(event)
         }
-    }
+    })
 })(budgetController, UIController);
 
 
-controller.anotherPublic();
+
